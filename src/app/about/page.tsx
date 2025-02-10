5c05634e5dc6486c17d3bd72784a06640c81166c
Bert Catsburg <bert@catsburg.com>
2025-02-10 21:31:38 +0100

    Layouts

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+const AboutPage = () => {
+    return (
+        <main>
+            <h1>About Us !!</h1>
+            <p><Link href="/">Back home</Link></p>
+        </main>
+    )
+}
+
+export default AboutPage
