201913cce17cf11767f21415f20b9249e256848e
Bert Catsburg <bert@catsburg.com>
2025-02-10 21:35:08 +0100

    Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
 // import Image from "next/image";
 // import styles from "./page.module.css";
 import Link from 'next/link';
+import {Image} from '@/components';
 
 export default function Home() {
     return (
         <main>
-            <img src="/logo.png" alt="A server surrounded by magic sparkles."/>
+            <Image />
             <h1>Welcome to this NextJS Course!</h1>
             <p>🔥 Let&apos;s get started! 🔥</p>
             <p><Link href="/about">About Us</Link></p>
         </main>
     );
 }
+// <img src="/logo.png" alt="A server surrounded by magic sparkles."/>
---
+++ src/components/Image.tsx
@@ -0,0 +1,5 @@
+export const Image = () => {
+    return (
+        <img src="/logo.png" alt="A server surrounded by magic sparkles."/>
+    )
+}
